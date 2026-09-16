012c259ab88a98a1b7dc8d536cd69979a6c2e473
craigblackman1-rgb <craig.blackman1@gmail.com>
2026-09-16 09:15:53 +0100

    chore: remove abandoned scratch-art.mjs stub (u26 WO-EF-068)

--- scratch-art.mjs
+++
@@ -1,4 +0,0 @@
-import { renderToStaticMarkup } from "react-dom/server";
-import React from "react";
-import { execSync } from "child_process";
-// transpile TSX on the fly via next's swc? simpler: use esbuild if present
